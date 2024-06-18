f66ad33ccd7abb77a70c19c18f8421806ad101cf
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-06-18 12:50:31 +0900

    Update participantcard.tsx

--- src/components/participantcard.tsx
+++ src/components/participantcard.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 export default function ParticipantCard({
   fullname,
   email,
@@ -14,15 +18,26 @@ export default function ParticipantCard({
     zip: string;
   };
 }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className='p-12 bg-gray-100 rounded-md space-y-7 text-lg'>
       <h1>{fullname}</h1>
-      <h2>{email}</h2>
-      <h3>{age}</h3>
-      <h4>{address.street}</h4>
-      <h4>{address.city}</h4>
-      <h4>{address.state}</h4>
-      <h4>{address.zip}</h4>
+      <button
+        className='p-2 rounded-md bg-blue-500 text-white'
+        onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "Collapse" : "Show more"}
+      </button>
+      {isExpanded && (
+        <>
+          <h2>{email}</h2>
+          <h3>{age}</h3>
+          <h4>{address.street}</h4>
+          <h4>{address.city}</h4>
+          <h4>{address.state}</h4>
+          <h4>{address.zip}</h4>
+        </>
+      )}
     </div>
   );
 }
